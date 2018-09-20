0616dd13a3f7bcc76e77e0ac807b088a9419e41b
Michal Sojka <michal.sojka@cvut.cz>
2018-09-20 16:54:10 +0200

    Fftw: Do not hide member variables inherited from Fft
    Fixes #22.

--- src/fft_fftw.h
+++ src/fft_fftw.h
@@ -25,7 +25,6 @@ public:
     void inverse(ComplexMat &complex_input, MatDynMem &real_result) override;
     ~Fftw() override;
 private:
-    unsigned m_width, m_height, m_num_of_feats, m_num_of_scales;
     cv::Mat m_window;
     fftwf_plan plan_f, plan_f_all_scales, plan_fw, plan_fw_all_scales, plan_i_features,
 	plan_i_features_all_scales, plan_i_1ch, plan_i_1ch_all_scales;
