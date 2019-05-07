0545cb4adc0df77fc212a9ccb7109d7a6948d88f
Michal Sojka <michal.sojka@cvut.cz>
2019-05-07 13:35:53 +0200

    Fix CUDA implementation of ComplexMat::sqr_norm
    Thanks to Flavio Kreiliger for noticing the bug.
    
    Fortunately, the difference between the correct result and our result
    was either zero or very small, so this had negligible effect on
    tracker accuracy.

--- src/complexmat.cu
+++ src/complexmat.cu
@@ -6,16 +6,17 @@ __global__ void sqr_norm_kernel(const float *in, float *block_res, int total)
     extern __shared__ float sdata[];
     int in_idx = 2 * (blockIdx.x * blockDim.x + threadIdx.x);
     int i = threadIdx.x;
+    unsigned ins = blockDim.x;
 
     if (in_idx >= total * 2)
         sdata[i] = 0;
     else
         sdata[i] = in[in_idx] * in[in_idx] + in[in_idx + 1] * in[in_idx + 1];
 
-    for (unsigned s = (blockDim.x + 1) / 2; s > 0; s >>= 1) {
+    for (unsigned outs = (ins + 1) / 2; ins > 1; ins = outs, outs = (outs + 1) / 2) {
         __syncthreads();
-        if (i < s)
-            sdata[i] += sdata[i + s];
+        if (i + outs < ins)
+            sdata[i] += sdata[i + outs];
     }
 
     if (i == 0)
